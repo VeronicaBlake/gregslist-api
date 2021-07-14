8739e1e669d18c49b143ce10e191368f9e066d00
Veronica Blake/Noyce <vrex149@gmail.com>
2021-07-14 13:59:23 -0600

    init

---
+++ app/Models/House.js
@@ -0,0 +1,11 @@
+export default class House{
+    constructor(){
+        this.bedrooms = bedrooms, 
+        this.bathrooms = bathrooms, 
+        this.isHoa = isHoa,
+        this.price = price, 
+        this.imageUrl = imageUrl || '//placehold.it/200x200'
+        this.description = description || 'no description given'
+
+    }
+}
