4534e29bbf2f93c2331fa1b949f3e7a9346d8ddf
Veronica Blake/Noyce <vrex149@gmail.com>
2021-07-20 16:16:27 -0600

    gettin them houses 🏠

--- app/Services/HousesService.js
+++ app/Services/HousesService.js
@@ -1,10 +1,31 @@
 import { ProxyState } from "../AppState.js"
 import House from "../Models/House.js"
+import { api } from './AxiosService.js'
 
 class HousesService{
-    createHouse(rawHouse){
-        ProxyState.houses = [...ProxyState.houses, new House(rawHouse)]
+    constructor(){
+        this.getAllHouses()
     }
+    async createHouse(rawHouse){
+        const res = await api.post('houses', rawHouse)
+        ProxyState.houses = [...ProxyState.houses, new House(res.data)]
+    }
+    async getAllHouses(){
+        try{
+            const res = await api.get('houses')
+            ProxyState.houses = res.data.map(h => new House(h))
+        }catch (error){
+            console.error(error)
+        }
+    }
+    async deleteHouse(houseId){
+        try{
+
+        }catch{
+            console.error(error)
+        }
+    }
+    
 }
 
 export const housesService = new HousesService()
